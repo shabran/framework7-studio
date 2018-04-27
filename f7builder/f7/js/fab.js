47ad0ed3b079029ba81a40d2d25a4d708d1345ce
aseprizal <aseprizal70@gmail.com>
2018-04-27 17:18:59 +0700

    fixing bug on linux and some module

---
+++ f7builder/f7/js/fab.js
@@ -0,0 +1,86 @@
+blockManager.add('fab link close', {
+    label: 'FAB Link',
+    attributes: {
+        class: 'fa fa-square',
+        title: 'FAB Link'
+    },
+    content:
+    '<div class="page">'+
+        '<div class="navbar">'+
+        '  <div class="navbar-inner">'+
+        '    <div class="title">Floating Action Button</div>'+
+        '  </div>'+
+        '</div>'+
+        '<div class="toolbar toolbar-bottom-md fab-morph-target">'+
+        '  <div class="toolbar-inner">'+
+        '    <a class="link fab-close">Link 1</a>'+
+        '    <a class="link fab-close">Link 2</a>'+
+        '    <a class="link fab-close">Link 3</a>'+
+        ' </div>'+
+        '</div>'+
+        '<div class="fab fab-left-top color-yellow">'+
+        '  <a href="#">'+
+        '    <i class="icon material-icons md-only">add</i>'+
+        '    <i class="icon material-icons md-only">close</i>'+
+        '  </a>'+
+        '  <div class="fab-buttons fab-buttons-bottom">'+
+        '    <a href="">1</a>'+
+        '    <a href="">2</a>'+
+        '    <a href="">3</a>'+
+        '  </div>'+
+        '</div>'+
+        '<div class="fab fab-right-top color-pink">'+
+        '  <a href="#">'+
+        '    <i class="icon material-icons md-only">add</i>'+
+        '    <i class="icon material-icons md-only">close</i>'+
+        '  </a>'+
+        '  <div class="fab-buttons fab-buttons-left">'+
+        '    <a href="">1</a>'+
+        '    <a href="">2</a>'+
+        '    <a href="">3</a>'+
+        '  </div>'+
+        '</div>'+
+        '<div class="fab fab-center-center color-green">'+
+        '  <a href="#">'+
+        '    <i class="icon material-icons md-only">add</i>'+
+        '    <i class="icon material-icons md-only">close</i>'+
+        '  </a>'+
+        '  <div class="fab-buttons fab-buttons-center">'+
+        '    <a href="">1</a>'+
+        '    <a href="">2</a>'+
+        '    <a href="">3</a>'+
+        '  </div>'+
+        '</div>'+
+        '<div class="fab fab-left-bottom color-orange">'+
+        '  <a href="#">'+
+        '    <i class="icon material-icons md-only">add</i>'+
+        '    <i class="icon material-icons md-only">close</i>'+
+        '  </a>'+
+        '  <div class="fab-buttons fab-buttons-top">'+
+        '    <a href="">1</a>'+
+        '    <a href="">2</a>'+
+        '    <a href="">3</a>'+
+        '  </div>'+
+        '</div>'+
+        '<div class="fab fab-right-bottom fab-morph" data-morph-to=".toolbar">'+
+        '  <a href="#">'+
+        '    <i class="icon material-icons md-only">add</i>'+
+        '    <i class="icon material-icons md-only">close</i>'+
+        '  </a>'+
+        '  <div class="fab-buttons fab-buttons-bottom">'+
+        '    <a href="">1</a>'+
+        '    <a href="">2</a>'+
+        '    <a href="">3</a>'+
+        '  </div>'+
+        '</div>'+
+        '<div class="page-content">'+
+        '  <div class="block">'+
+        '    <p>Lorem ipsum dolor sit amet...</p>'+
+        '  </div>'+
+        '</div>'+
+    '</div>',
+    category: {
+        label: 'Floating Action Buttons',
+        open: false
+    }
+});
