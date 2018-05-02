9e6f94f517e889efb5223ee0072242b7e7750b47
shabran <shabran.ahmad@gmail.com>
2018-05-02 09:43:22 +0700

    add 4 function

---
+++ f7builder/f7/js_backup/app.js
@@ -0,0 +1,7 @@
+var $$ = Dom7;
+
+var app = new Framework7({
+    root: '#app',
+    name: 'NowDB Application Creator',
+    id: 'nowdb.app.creator'
+});
---
+++ f7builder/f7/js_backup/layout.js
@@ -0,0 +1,19 @@
+blockManager.add('single-view', {
+    label: 'Single Page',
+    attributes: {
+        class: 'fa fa-square',
+        title: 'Single Page'
+    },
+    content: '<div class="page"></div>',
+    category: 'Layout'
+});
+
+blockManager.add('page-content', {
+    label: 'Page Content',
+    attributes: {
+        class: 'fa fa-square',
+        title: 'Page Content'
+    },
+    content: '<div class="page-content"></div>',
+    category: 'Layout'
+});
